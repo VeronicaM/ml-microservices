24b392112f5eba547804bb3c1fe697f6b05852ce
Veronica Mihai <mihaiveronica@ymail.com>
2020-04-11 19:20:23 +0300

    add upload_docker commands

--- upload_docker.sh
+++ upload_docker.sh
@@ -5,11 +5,16 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=veronicam/ml-services:1.0
 
 # Step 2:  
 # Authenticate & tag
+docker login --username=veronicam --email=veronica.mihai90@outlook.com
 echo "Docker ID and Image: $dockerpath"
+docker images
+
+docker tag 076273b441c0 veronicam/ml-services:1.0
 
 # Step 3:
 # Push image to a docker repository
+docker push veronicam/ml-services:1.0
